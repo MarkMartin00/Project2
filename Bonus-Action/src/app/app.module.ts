bd578d849fbb71b385d9cd5ac05a2d10679b2c23
Mark Martin <95247709+MarkMartin00@users.noreply.github.com>
2022-01-19 18:09:53 -0600

    Mark- folders

--- Bonus-Action/src/app/app.module.ts
+++ Bonus-Action/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
+import { LoginComponent } from './components/login/login.component';
+import { CreationComponent } from './components/creation/creation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    LoginComponent,
+    CreationComponent
   ],
   imports: [
     BrowserModule,
