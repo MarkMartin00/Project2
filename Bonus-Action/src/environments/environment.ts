ff7f60becf02c45fa47d156c5e08a2a6eb2cee1c
Cristopher Aracena <aracenacristopher@gmail.com>
2022-01-21 11:45:03 -0500

    Update environment.ts

--- Bonus-Action/src/environments/environment.ts
+++ Bonus-Action/src/environments/environment.ts
@@ -6,6 +6,8 @@ export const environment = {
   production: false
 };
 
+export const apiUrl = `https://www.dnd5eapi.co/api/`
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
@@ -14,4 +16,3 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
-export const awsUrl = `http://completeapibeanstalk-env.eba-sfn83npg.us-east-1.elasticbeanstalk.com/api`
