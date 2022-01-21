05d405fcca9d022e86fd7a412c9ba061346b48ce
mikeoliver14 <michaelsheaoliver@gmail.com>
2022-01-21 14:21:51 -0700

    Mike - DB connection

---
+++ Project2/src/main/java/com/revature/controller/UserController.java
@@ -0,0 +1,52 @@
+package com.revature.controller;
+
+import java.util.Set;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.models.User;
+import com.revature.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	UserService userServ;
+	
+	@GetMapping
+	public Set<User> getAll() {
+		return userServ.findAll();
+	}
+	
+	@GetMapping("/find/{username}")
+	public ResponseEntity<User> findByUsername(@PathVariable("username") String username) {
+		return ResponseEntity.ok(userServ.getByUsername(username));
+	}
+	
+	@PostMapping("/add")
+	public ResponseEntity<User> addUser(@Valid @RequestBody User u) {
+		return ResponseEntity.ok(userServ.add(u));
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findUserById(@PathVariable("id") int id) {
+		return ResponseEntity.ok(userServ.getById(id));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void removeUser(@PathVariable("id") int id) {
+		userServ.remove(id);
+	}
+
+}
