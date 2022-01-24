e198559fd9f51b7466179ab50615ed56a12ecf7d
Mark Martin <95247709+MarkMartin00@users.noreply.github.com>
2022-01-24 12:48:38 -0600

    Mark - alg for attack rolls and attack numbers

--- Bonus-Action/src/app/models/player-character.ts
+++ Bonus-Action/src/app/models/player-character.ts
@@ -22,6 +22,10 @@ export class PlayerCharacter {
   char_skills: Map<string, number>;
   // Spells
   char_spells: Spell[];
+  feature_name: string;
+  equipment_name: string;
+  magical_equipment: any;
+  equipment_proficiencies: any;
 
   constructor(
     char_name: string,
@@ -112,84 +116,176 @@ export class PlayerCharacter {
   // STAT AUTOMATION
   public calcAC() {
     if ((this.char_class = 'Barbarian')) {
-      if ((this.char_features = 'Unarmored_Defense') && (this.char_equipment = 'shield')) {
-        this.char_ac += 10 + 2 + this.getStatModifier(this.char_stats.get("dex")) + this.getStatModifier(this.char_stats.get("con"));
+      if (
+        (this.feature_name = 'Unarmored_Defense') &&
+        (this.equipment_name = 'shield')
+      ) {
+        this.char_ac +=
+          10 +
+          2 +
+          this.getStatModifier(this.char_stats.get('dex')) +
+          this.getStatModifier(this.char_stats.get('con'));
         return this.char_ac;
-      } else if (this.char_features = 'Unarmored Defense') {
-        this.char_ac += 10 + this.getStatModifier(this.char_stats.get("dex")) + this.getStatModifier(this.char_stats.get("con"));
+      } else if ((this.feature_name = 'Unarmored_Defense')) {
+        this.char_ac +=
+          10 +
+          this.getStatModifier(this.char_stats.get('dex')) +
+          this.getStatModifier(this.char_stats.get('con'));
         return this.char_ac;
       }
     } else if ((this.char_class = 'Monk')) {
-      if ((this.char_features = 'Unarmored_Defense') && (this.char_equipment = 'shield')) {
-        this.char_ac += 10 + 2 + this.getStatModifier(this.char_stats.get("dex")) + this.getStatModifier(this.char_stats.get("wis"));
+      if (
+        (this.feature_name = 'Unarmored_Defense') &&
+        (this.equipment_name = 'shield')
+      ) {
+        this.char_ac +=
+          10 +
+          2 +
+          this.getStatModifier(this.char_stats.get('dex')) +
+          this.getStatModifier(this.char_stats.get('wis'));
         return this.char_ac;
-      } else if (this.char_features = 'Unarmored Defense') {
-        this.char_ac += 10 + this.getStatModifier(this.char_stats.get("dex")) + this.getStatModifier(this.char_stats.get("wis"));
+      } else if ((this.feature_name = 'Unarmored_Defense')) {
+        this.char_ac +=
+          10 +
+          this.getStatModifier(this.char_stats.get('dex')) +
+          this.getStatModifier(this.char_stats.get('wis'));
         return this.char_ac;
       }
     } else if ((this.char_class = '')) {
-      if ((this.char_equipment = 'padded_armor') && (this.char_equipment = 'shield')) {
-        this.char_ac += 12 + 2 + this.getStatModifier(this.char_stats.get("dex")) + this.magical_equipment.get("plus");
+      if (
+        (this.equipment_name = 'padded_armor') &&
+        (this.equipment_name = 'shield')
+      ) {
+        this.char_ac +=
+          12 +
+          2 +
+          this.getStatModifier(this.char_stats.get('dex')) +
+          this.magical_equipment.get('plus');
         return this.char_ac;
-      } else if ((this.char_equipment = 'padded_armor')) {
-        this.char_ac += 12 + this.getStatModifier(this.char_stats.get("dex")) + this.magical_equipment.get("plus");
+      } else if ((this.equipment_name = 'padded_armor')) {
+        this.char_ac +=
+          12 +
+          this.getStatModifier(this.char_stats.get('dex')) +
+          this.magical_equipment.get('plus');
         return this.char_ac;
-      } else if ((this.char_equipment = 'light_armor') && (this.char_equipment = 'shield')) {
-        this.char_ac += 11 + 2 + this.getStatModifier(this.char_stats.get("dex")) + this.magical_equipment.get("plus");
+      } else if (
+        (this.equipment_name = 'studded_armor') &&
+        (this.equipment_name = 'shield')
+      ) {
+        this.char_ac +=
+          12 +
+          2 +
+          this.getStatModifier(this.char_stats.get('dex')) +
+          this.magical_equipment.get('plus');
         return this.char_ac;
-      } else if ((this.char_equipment = 'light_armor')) {
-        this.char_ac += 11 + this.getStatModifier(this.char_stats.get("dex")) + this.magical_equipment.get("plus");
+      } else if ((this.equipment_name = 'studded_armor')) {
+        this.char_ac +=
+          12 +
+          this.getStatModifier(this.char_stats.get('dex')) +
+          this.magical_equipment.get('plus');
         return this.char_ac;
-      } else if ((this.char_equipment = 'hide_armor') && (this.char_equipment = 'shield')) {
-        this.char_ac += 12 + 2 + this.statModifierMax() + this.magical_equipment.get("plus");
+      } else if (
+        (this.equipment_name = 'light_armor') &&
+        (this.equipment_name = 'shield')
+      ) {
+        this.char_ac +=
+          11 +
+          2 +
+          this.getStatModifier(this.char_stats.get('dex')) +
+          this.magical_equipment.get('plus');
         return this.char_ac;
-      } else if ((this.char_equipment = 'hide_armor')) {
-        this.char_ac += 12 + this.statModifierMax() + this.magical_equipment.get("plus");
+      } else if ((this.equipment_name = 'light_armor')) {
+        this.char_ac +=
+          11 +
+          this.getStatModifier(this.char_stats.get('dex')) +
+          this.magical_equipment.get('plus');
         return this.char_ac;
-      } else if ((this.char_equipment = 'chain_shirt') && (this.char_equipment = 'shield')) {
-        this.char_ac += 13 + 2 + this.statModifierMax() + this.magical_equipment.get("plus");
+      } else if (
+        (this.equipment_name = 'hide_armor') &&
+        (this.equipment_name = 'shield')
+      ) {
+        this.char_ac +=
+          12 + 2 + this.statModifierMax() + this.magical_equipment.get('plus');
         return this.char_ac;
-      } else if ((this.char_equipment = 'chain_shirt')) {
-        this.char_ac += 13 + this.statModifierMax() + this.magical_equipment.get("plus");
+      } else if ((this.equipment_name = 'hide_armor')) {
+        this.char_ac +=
+          12 + this.statModifierMax() + this.magical_equipment.get('plus');
         return this.char_ac;
-      } else if ((this.char_equipment = 'half_plate') && (this.char_equipment = 'shield')) {
-        this.char_ac += 15 + 2 + this.statModifierMax() + this.magical_equipment.get("plus");
+      } else if (
+        (this.equipment_name = 'chain_shirt') &&
+        (this.equipment_name = 'shield')
+      ) {
+        this.char_ac +=
+          13 + 2 + this.statModifierMax() + this.magical_equipment.get('plus');
         return this.char_ac;
-      } else if ((this.char_equipment = 'half_plate')) {
-        this.char_ac += 15 + this.statModifierMax() + this.magical_equipment.get("plus");
+      } else if ((this.equipment_name = 'chain_shirt')) {
+        this.char_ac +=
+          13 + this.statModifierMax() + this.magical_equipment.get('plus');
         return this.char_ac;
-      } else if (((this.char_equipment = 'scale_mail') || (this.char_equipment = 'breastplate_armor') || (this.char_equipment = 'ring_mail')) && (this.char_equipment = 'shield')) {
-        this.char_ac += 14 + 2 + this.magical_equipment.get("plus");
+      } else if (
+        (this.equipment_name = 'half_plate') &&
+        (this.equipment_name = 'shield')
+      ) {
+        this.char_ac +=
+          15 + 2 + this.statModifierMax() + this.magical_equipment.get('plus');
         return this.char_ac;
-      } else if ((this.char_equipment = 'scale_mail') || (this.char_equipment = 'breastplate_armor') || (this.char_equipment = 'ring_mail')) {
-        this.char_ac += 14 + this.magical_equipment.get("plus");
+      } else if ((this.equipment_name = 'half_plate')) {
+        this.char_ac +=
+          15 + this.statModifierMax() + this.magical_equipment.get('plus');
         return this.char_ac;
-      } else if ((this.char_equipment = 'chain_mail') && (this.char_equipment = 'shield')) {
-        this.char_ac += 16 + 2 + this.magical_equipment.get("plus");
+      } else if (
+        ((this.equipment_name = 'scale_mail') ||
+          (this.equipment_name = 'breastplate_armor') ||
+          (this.equipment_name = 'ring_mail')) &&
+        (this.equipment_name = 'shield')
+      ) {
+        this.char_ac += 14 + 2 + this.magical_equipment.get('plus');
         return this.char_ac;
-      } else if ((this.char_equipment = 'chain_mail')) {
-        this.char_ac += 16 + this.magical_equipment.get("plus");
+      } else if (
+        (this.equipment_name = 'scale_mail') ||
+        (this.equipment_name = 'breastplate_armor') ||
+        (this.equipment_name = 'ring_mail')
+      ) {
+        this.char_ac += 14 + this.magical_equipment.get('plus');
         return this.char_ac;
-      } else if ((this.char_equipment = 'splint_armor') && (this.char_equipment = 'shield')) {
-        this.char_ac += 17 + 2 + this.magical_equipment.get("plus");
+      } else if (
+        (this.equipment_name = 'chain_mail') &&
+        (this.equipment_name = 'shield')
+      ) {
+        this.char_ac += 16 + 2 + this.magical_equipment.get('plus');
         return this.char_ac;
-      } else if ((this.char_equipment = 'splint_armor')) {
-        this.char_ac += 17 + this.magical_equipment.get("plus");
+      } else if ((this.equipment_name = 'chain_mail')) {
+        this.char_ac += 16 + this.magical_equipment.get('plus');
         return this.char_ac;
-      } else if ((this.char_equipment = 'plate_armor') && (this.char_equipment = 'shield')) {
-        this.char_ac += 18 + 2 + this.magical_equipment.get("plus");
+      } else if (
+        (this.equipment_name = 'splint_armor') &&
+        (this.equipment_name = 'shield')
+      ) {
+        this.char_ac += 17 + 2 + this.magical_equipment.get('plus');
         return this.char_ac;
-      } else if ((this.char_equipment = 'plate_armor')) {
-        this.char_ac += 18 + this.magical_equipment.get("plus");
+      } else if ((this.equipment_name = 'splint_armor')) {
+        this.char_ac += 17 + this.magical_equipment.get('plus');
         return this.char_ac;
+      } else if (
+        (this.equipment_name = 'plate_armor') &&
+        (this.equipment_name = 'shield')
+      ) {
+        this.char_ac += 18 + 2 + this.magical_equipment.get('plus');
+        return this.char_ac;
+      } else if ((this.equipment_name = 'plate_armor')) {
+        this.char_ac += 18 + this.magical_equipment.get('plus');
+        return this.char_ac;
+      } else {
+        this.char_ac += 10 + this.getStatModifier(this.char_stats.get('dex'));
       }
-    }}
+    }
+  }
 
   public statModifierMax() {
-    if (this.getStatModifier(this.char_stats.get("dex")) >= 2) {
+    if (this.getStatModifier(this.char_stats.get('dex')) >= 2) {
       return 2;
     } else {
-      return this.getStatModifier(this.char_stats.get("dex"));
+      return this.getStatModifier(this.char_stats.get('dex'));
     }
   }
 
@@ -264,6 +360,251 @@ export class PlayerCharacter {
         return 0;
     }
   }
+  public d4() {
+    var d4 = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+    return d4;
+  }
+  public d6() {
+    var d6 = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+    return d6;
+  }
+  public d8() {
+    var d8 = Math.floor(Math.random() * (8 - 1 + 1)) + 1;
+    return d8;
+  }
+  public d10() {
+    var d10 = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+    return d10;
+  }
+  public d12() {
+    var d12 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    return d12;
+  }
+  public d20() {
+    var d20 = Math.floor(Math.random() * (20 - 1 + 1)) + 1;
+    return d20;
+  }
+
+  public meleeAttackRoll(martial:boolean, simple:boolean) {
+    if ((this.equipment_proficiencies.includes(martial=true)) || (this.equipment_proficiencies.includes(simple=true))) {
+      let attack = d20() + this.getProficiencyMod() + this.getStatModifier();
+      alert(
+        'Your attack roll is a ${d20} with an added proficiency bonus of ${this.getProficiencyMod() and your ability modifier of ${this.getStatModifier()} which totals ${attack}'
+      );
+    } else {
+      let attack = d20() + this.getStatModifier();
+      alert(
+        'Your attack roll is a ${d20} with  your ability modifier of ${this.getStatModifier()} since you are not proficient with this weapon. This totals ${attack}'
+      );
+    }
+  }
+
+  public meleeAttack() {
+    let damage: number;
+    if (
+      (this.equipment_name = 'Dagger') ||
+      (this.equipment_name = 'Dart') ||
+      (this.equipment_name = 'Whip')
+    ) {
+      damage =
+        this.d4() +
+        this.getStatModifier(this.char_stats.get('dex')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} piercing damage!");
+      return damage;
+    } else if (
+      (this.equipment_name = 'Scrimitar') ||
+      (this.equipment_name = 'Handaxe')
+    ) {
+      damage =
+        this.d6() +
+        this.getStatModifier(this.char_stats.get('str')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} slashing damage!");
+      return damage;
+    } else if (
+      (this.equipment_name = 'Club') ||
+      (this.equipment_name = 'Light-Hammer')
+    ) {
+      damage =
+        this.d4() +
+        this.getStatModifier(this.char_stats.get('str')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} bludgeoning damage!");
+      return damage;
+    } else if (
+      (this.equipment_name = 'Shortsword') ||
+      (this.equipment_name = 'Javelin')
+    ) {
+      damage =
+        this.d6() +
+        this.getStatModifier(this.char_stats.get('str')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} piercing damage!");
+      return damage;
+    } else if (
+      (this.equipment_name = 'Mace') ||
+      (this.equipment_name = 'Quarterstaff-1hand')
+    ) {
+      damage =
+        this.d6() +
+        this.getStatModifier(this.char_stats.get('str')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} bludgeoning damage!");
+      return damage;
+    } else if ((this.equipment_name = 'Sickle')) {
+      damage =
+        this.d4() +
+        this.getStatModifier(this.char_stats.get('str')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} slashing damage!");
+      return damage;
+    } else if (
+      (this.equipment_name = 'Spear-1hand') ||
+      (this.equipment_name = 'Trident-1hand')
+    ) {
+      damage =
+        this.d6() +
+        this.getStatModifier(this.char_stats.get('str')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} piercing damage!");
+      return damage;
+    } else if (
+      (this.equipment_name = 'Spear-2hand') ||
+      (this.equipment_name = 'Morningstar')
+    ) {
+      damage =
+        this.d8() +
+        this.getStatModifier(this.char_stats.get('str')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} piercing damage!");
+      return damage;
+    } else if (
+      (this.equipment_name = 'Light-Crossbow') ||
+      (this.equipment_name = 'Rapier') ||
+      (this.equipment_name = 'Longbow')
+    ) {
+      damage =
+        this.d8() +
+        this.getStatModifier(this.char_stats.get('dex')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} piercing damage!");
+      return damage;
+    } else if (
+      (this.equipment_name = 'Shortbow') ||
+      (this.equipment_name = 'Hand-Crossbow')
+    ) {
+      damage =
+        this.d6() +
+        this.getStatModifier(this.char_stats.get('dex')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} piercing damage!");
+      return damage;
+    } else if ((this.equipment_name = 'Sling')) {
+      damage =
+        this.d4() +
+        this.getStatModifier(this.char_stats.get('dex')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} bludgeoning damage!");
+      return damage;
+    } else if ((this.equipment_name = 'Heavy-Crossbow')) {
+      damage =
+        this.d10() +
+        this.getStatModifier(this.char_stats.get('dex')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} piercing damage!");
+      return damage;
+    } else if (
+      (this.equipment_name = 'Battleaxe-1hand') ||
+      (this.equipment_name = 'Longsword-1hand')
+    ) {
+      damage =
+        this.d8() +
+        this.getStatModifier(this.char_stats.get('str')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} slashing damage!");
+      return damage;
+    } else if (
+      (this.equipment_name = 'Flail') ||
+      (this.equipment_name = 'Warhammer-1hand') ||
+      (this.equipment_name = 'Quarterstaff-2hand') ||
+      (this.equipment_name = 'Greatclub')
+    ) {
+      damage =
+        this.d8() +
+        this.getStatModifier(this.char_stats.get('str')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} bludgeoning damage!");
+      return damage;
+    } else if (
+      (this.equipment_name = 'Glaive-2hand') ||
+      (this.equipment_name = 'Longsword-2hand') ||
+      (this.equipment_name = 'Halberd-2hand') ||
+      (this.equipment_name = 'Battleaxe-2hand')
+    ) {
+      damage =
+        this.d10() +
+        this.getStatModifier(this.char_stats.get('str')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} slashing damage!");
+      return damage;
+    } else if ((this.equipment_name = 'Greataxe-2hand')) {
+      damage =
+        this.d12() +
+        this.getStatModifier(this.char_stats.get('str')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} slashing damage!");
+      return damage;
+    } else if ((this.equipment_name = 'Greatsword-2hand')) {
+      damage =
+        this.d6() +
+        this.d6() +
+        this.getStatModifier(this.char_stats.get('str')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} bludgeoning damage!");
+      return damage;
+    } else if ((this.equipment_name = 'Lance-2hand')) {
+      damage =
+        this.d12() +
+        this.getStatModifier(this.char_stats.get('str')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} piercing damage!");
+      return damage;
+    } else if ((this.equipment_name = 'Maul-2hand')) {
+      damage =
+        this.d6() +
+        this.d6() +
+        this.getStatModifier(this.char_stats.get('str')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} bludgeoning damage!");
+      return damage;
+    } else if ((this.equipment_name = 'Pike-2hand')) {
+      damage =
+        this.d10() +
+        this.getStatModifier(this.char_stats.get('str')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} piercing damage!");
+      return damage;
+    } else if (
+      (this.equipment_name = 'Trident-2hand') ||
+      (this.equipment_name = 'War-pick')
+    ) {
+      damage =
+        this.d8() +
+        this.getStatModifier(this.char_stats.get('str')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} piercing damage!");
+      return damage;
+    } else if ((this.equipment_name = 'Warhammer-2hand')) {
+      damage =
+        this.d10() +
+        this.getStatModifier(this.char_stats.get('str')) +
+        this.magical_equipment.get('plus');
+      alert("You've done ${damage} bludgeoning damage!");
+      return damage;
+    }
+    return alert('You did 1 bludgeoning damage with an unarmed strike!');
+  }
 
   public getAbilityStat(skill: string) {
     if (
@@ -299,3 +640,5 @@ export class PlayerCharacter {
     } else return '';
   }
 }
+
+
